b4df525b6f24e41f217530bc12e25e99b06362f5
KaiWelu <kaiweluda@gmail.com>
2024-09-11 16:49:57 +0200

    adds crud for users

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const app = express();
 
+//json - this is important for parsing requests in json
+app.use(express.json());
+
 // normally here would be the body parser middleware but it's build in from express 4.16 on
 // the others may be needed in the future
 /* app.use(compression());
@@ -25,8 +28,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-
-//testing if the app works
+//testing if the express server works
 app.get("/test", (req, res) => {
   try {
     res.status(200).json({ message: "API is working :)" });
@@ -34,3 +36,79 @@ app.get("/test", (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get one user by an id
+app.get("/users/:id", async (req, res) => {
+  try {
+    const users = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// create a user
+app.post("/users", async (req, res) => {
+  try {
+    console.log(req);
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update user
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//delete user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
