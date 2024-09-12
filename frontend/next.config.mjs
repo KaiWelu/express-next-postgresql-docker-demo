b836ecfc6effaf2ee4edec6f02c8490b8302022f
KaiWelu <kaiweluda@gmail.com>
2024-09-12 15:09:47 +0200

    dockerizes the frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //this is important to deploy with docker
+  output: "standalone",
+};
 
 export default nextConfig;
